b90a2a5c11fc9efa31c4644348f0851cd3d15bea
FedeCossia 🥴 <68232817+fedexcc@users.noreply.github.com>
2022-01-11 13:38:58 -0300

    Fix?

--- admin/src/components/quiz/edition.js
+++ admin/src/components/quiz/edition.js
@@ -77,6 +77,11 @@ const Edition = (props) => {
         })
         getQuiz()
     }
+    
+    const getQuiz = async () => {
+        const res = await QuizService.findById(props.record.id)
+        setQuiz(res.data)
+    }
 
     const sendAnswer = async () => {
         const res = await AnswerService.create({
@@ -84,12 +89,8 @@ const Edition = (props) => {
             value:newAnswer.value,
             questionId:question.id
         })
+        setOpenA(false)
         getQuiz()
-    }
-    
-    const getQuiz = async () => {
-        const res = await QuizService.findById(props.record.id)
-        setQuiz(res.data)
     }
 
     const deleteQ = async (q) => {
