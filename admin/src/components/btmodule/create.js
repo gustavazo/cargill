161c17eb9c07925d858104b9f277ee766c71a323
Ramiro Trovant <ramiro.trovant@rayo.com>
2022-01-20 16:27:28 +0000

    stala

---
+++ admin/src/components/btmodule/create.js
@@ -0,0 +1,23 @@
+// input de name
+// input de mac address
+// select de areas
+    // tenes que traer las areas del backend y que esas sean las opciones para seleccionar
+    // el valor del select deberia ser el id del area
+
+
+{/* <FormControl fullWidth>
+  <InputLabel id="demo-simple-select-label">Area</InputLabel>
+  <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    value={areaId}
+    label="Age"
+    onChange={handleChangeArea}
+  >
+    {
+        areas.map(a => (
+            <MenuItem value={a.id}>{a.name}</MenuItem>
+        ))
+    }
+  </Select>
+</FormControl> */}
