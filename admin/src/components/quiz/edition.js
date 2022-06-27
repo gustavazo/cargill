4c40dfd93e5be00e95f0ec10d3920c6f21f60622
LucasMarelli <lucasmarelli90@gmail.com>
2022-06-27 15:57:55 -0300

    Refresh
    1-"Edition" component: getQuiz()  added on sendAnwer function to update the quiz state of the component
    2-"QuestionAnswer" component: Added a "useEffect" to be notified of question modifications in the parent. Allows updating the questions state of the component when there are modifications in the parent

--- admin/src/components/quiz/edition.js
+++ admin/src/components/quiz/edition.js
@@ -30,16 +30,16 @@ const Edition = (props) => {
     )
     const [editQ, setEditQ] = useState({
         statement: '',
-        id:0
+        id: 0
     })
     const [newAnswer, setNewAnswer] = useState({
-        answer:'',
-        value:true
+        answer: '',
+        value: true
     })
     const [quiz, setQuiz] = useState(props.record)
     const [question, setQuestion] = useState()
 
-    console.log(quiz,'e logi')
+    console.log(quiz, 'quiz')
 
     const onRowClick = (q) => {
         if (questionSelected === null) {
@@ -49,24 +49,24 @@ const Edition = (props) => {
         } else {
             setQuestionSelected(null)
         }
-        
-        
+
+
     }
 
     const openModalQ = () => {
         setOpenQ(true)
     }
-    
+
     const closeModalQ = () => {
         setOpenQ(false)
     }
-    
+
     const openModalA = (q) => {
         setOpenA(true)
         setQuestion(q)
         setQuestionSelected(null)
     }
-    
+
     const closeModalA = () => {
         setOpenA(false)
         getQuiz()
@@ -91,7 +91,7 @@ const Edition = (props) => {
         setNewAnswer({
             ...newAnswer,
             answer: e.target.value
-            })  
+        })
     }
 
     const handleChangeE = (e) => {
@@ -103,14 +103,14 @@ const Edition = (props) => {
 
     const sendQuestion = async () => {
         const res = await QuestionService.create({
-            statement:newQuestion,
+            statement: newQuestion,
             quizId: quiz.id
         })
         getQuiz()
         closeModalQ()
         notify('La categoría ha sido creada')
     }
-    
+
     const getQuiz = async () => {
         const res = await QuizService.findById(props.record.id)
         setQuiz(res.data)
@@ -118,13 +118,17 @@ const Edition = (props) => {
 
     const sendAnswer = async () => {
         const res = await AnswerService.create({
-            label:newAnswer.answer,
-            value:newAnswer.value,
-            questionId:question.id
+            label: newAnswer.answer,
+            value: newAnswer.value,
+            questionId: question.id
         })
-        setOpenA(false)
-        window.location.reload()
+        console.log('sendAnwerResp',res)
+        // setTimeout(getQuiz,5000)
+        setOpenA(false);
+        getQuiz()
         notify('La consigna ha sido creada')
+         
+
     }
 
     const deleteQ = async (q) => {
@@ -134,27 +138,27 @@ const Edition = (props) => {
     }
 
     const editedQ = async () => {
-        const res = await QuestionService.editQuestion(editQ.id,{statement: editQ.statement})
+        const res = await QuestionService.editQuestion(editQ.id, { statement: editQ.statement })
         getQuiz()
         closeModalE()
         notify('La categoría ha sido editada')
-        
+
     }
 
-    
+
 
     return (
 
         <div>
-            <div style={{fontSize: 35, margin: 20, marginBottom: 0, fontWeight:'bold'}}>{quiz?.title}</div>
-            <div style={{fontSize: 30, margin: 20, marginTop: 0, fontWeight: 'lighter'}}>{quiz?.description}</div>
-            <Button style={{position:'absolute', top:100, right: 40}}onClick={openModalQ}  variant="contained" color='primary'>Agregar Categoría</Button>
+            <div style={{ fontSize: 35, margin: 20, marginBottom: 0, fontWeight: 'bold' }}>{quiz?.title}</div>
+            <div style={{ fontSize: 30, margin: 20, marginTop: 0, fontWeight: 'lighter' }}>{quiz?.description}</div>
+            <Button style={{ position: 'absolute', top: 100, right: 40 }} onClick={openModalQ} variant="contained" color='primary'>Agregar Categoría</Button>
             <Modal
-            show={openE}
-            onClose={closeModalE}
+                show={openE}
+                onClose={closeModalE}
             >
-                <div style={{display: 'flex', justifyContent: 'center', color: 'black', fontWeight: 'bold', fontSize: 20, padding: 10}}>Editar categoria</div>
-                <form style={{display: 'flex', justifyContent: 'center'}}>
+                <div style={{ display: 'flex', justifyContent: 'center', color: 'black', fontWeight: 'bold', fontSize: 20, padding: 10 }}>Editar categoria</div>
+                <form style={{ display: 'flex', justifyContent: 'center' }}>
                     <TextField
                         label="Cateogría"
                         margin="normal"
@@ -164,16 +168,16 @@ const Edition = (props) => {
                         onChange={handleChangeE}
                     />
                 </form>
-                <div style={{display: 'flex', justifyContent: 'center'}}>
-                <Button  variant='contained' color='primary' onClick={editedQ}>Enviar</Button>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Button variant='contained' color='primary' onClick={editedQ}>Enviar</Button>
                 </div>
             </Modal>
             <Modal
-            show={openQ}
-            onClose={closeModalQ}
+                show={openQ}
+                onClose={closeModalQ}
             >
-                <div style={{display: 'flex', justifyContent: 'center', color: 'black', fontWeight: 'bold', fontSize: 20, padding: 10}}>Agregar Categoría</div>
-                <form style={{display: 'flex', justifyContent: 'center'}}>
+                <div style={{ display: 'flex', justifyContent: 'center', color: 'black', fontWeight: 'bold', fontSize: 20, padding: 10 }}>Agregar Categoría</div>
+                <form style={{ display: 'flex', justifyContent: 'center' }}>
                     <TextField
                         label="Categoría"
                         margin="normal"
@@ -182,16 +186,16 @@ const Edition = (props) => {
                         onChange={handleChangeQ}
                     />
                 </form>
-                <div style={{display: 'flex', justifyContent: 'center'}}>
-                   <Button variant='contained' color='primary' onClick={sendQuestion}>Enviar</Button>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Button variant='contained' color='primary' onClick={sendQuestion}>Enviar</Button>
                 </div>
             </Modal>
             <Modal
-            show={openA}
-            onClose={closeModalA}
+                show={openA}
+                onClose={closeModalA}
             >
-                <div style={{display: 'flex', justifyContent: 'center', color: 'black', fontWeight: 'bold', fontSize: 20, padding: 10}}>Agregar Consigna</div>
-                <form style={{display: 'flex', justifyContent: 'center'}}>
+                <div style={{ display: 'flex', justifyContent: 'center', color: 'black', fontWeight: 'bold', fontSize: 20, padding: 10 }}>Agregar Consigna</div>
+                <form style={{ display: 'flex', justifyContent: 'center' }}>
                     <TextField
                         label="Consigna"
                         margin="normal"
@@ -200,35 +204,35 @@ const Edition = (props) => {
                         onChange={handleChangeA}
                     />
                 </form>
-                <div style={{display: 'flex', justifyContent: 'center'}}>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <Button variant='contained' color='primary' onClick={sendAnswer}>Enviar</Button>
                 </div>
             </Modal>
-                    <div style={{display: 'flex', justifyContent: 'space-between', background: '#2E7D32', color: 'white', padding: 5}}>
-                        <div align="center" style={{fontSize: 22, fontWeight:'bold', marginLeft: 15}}>Categorías</div>
-                    </div>
-                    <div>
-                        {quiz.questions?.map((q) => (
-                            <>
-                                <div style={{cursor:'pointer'}} onClick={() => onRowClick(q)}>
-                                    <div style={{fontSize:18, display: 'flex', justifyContent:'space-between', margin: 10, marginLeft: 20}}>
-                                        <div>
-                                            {q.statement}
-                                        </div>
-                                        <div>
-                                            <Button onClick={() => openModalA(q)} variant="contained" color='success' style={{marginRight: 5}}>Agregar Consigna</Button>
-                                            <Button onClick={() => openModalE(q)} variant="contained" color='primary' style={{marginLeft: 5}}>Renombrar Categoría</Button>
-                                            <Button onClick={() => deleteQ(q)}variant="contained" color='error' style={{marginLeft: 5}}><DeleteIcon/></Button>
-                                        </div>
-                                    </div>
+            <div style={{ display: 'flex', justifyContent: 'space-between', background: '#2E7D32', color: 'white', padding: 5 }}>
+                <div align="center" style={{ fontSize: 22, fontWeight: 'bold', marginLeft: 15 }}>Categorías</div>
+            </div>
+            <div>{/* map de categorias */}
+                {quiz.questions?.map((q) => (
+                    <>
+                        <div style={{ cursor: 'pointer' }} onClick={() => onRowClick(q)}>
+                            <div style={{ fontSize: 18, display: 'flex', justifyContent: 'space-between', margin: 10, marginLeft: 20 }}>
+                                <div>
+                                    {q.statement}
                                 </div>
-                                <div style={{background: '#f5f5f4', paddingLeft: 20}}>
-                                  <QuestionAnswer questionSelected={q} />
+                                <div>
+                                    <Button onClick={() => openModalA(q)} variant="contained" color='success' style={{ marginRight: 5 }}>Agregar Consigna</Button>
+                                    <Button onClick={() => openModalE(q)} variant="contained" color='primary' style={{ marginLeft: 5 }}>Renombrar Categoría</Button>
+                                    <Button onClick={() => deleteQ(q)} variant="contained" color='error' style={{ marginLeft: 5 }}><DeleteIcon /></Button>
                                 </div>
+                            </div>
+                        </div>{/* por cada categoria estan sus consignas, se pasan a traves de la categoria */}
+                        <div style={{ background: '#f5f5f4', paddingLeft: 20 }}>
+                            <QuestionAnswer questionSelected={q} quiz = {quiz}/>
+                        </div>
 
-                            </>
-                        ))}
-                    </div>
+                    </>
+                ))}
+            </div>
         </div>
 
     )
--- admin/src/components/quiz/questionAnswer.js
+++ admin/src/components/quiz/questionAnswer.js
@@ -17,20 +17,26 @@ const QuestionAnswer = (props) => {
     const notify = useNotify();
     const [open, setOpen] = useState(false)
     const [editA, setEditA] = useState({
-        label:'',
-        id:0
+        label: '',
+        id: 0
     })
     const [answers, setAnswers] = useState(props.questionSelected);
     const [switchState, setSwitchState] = useState(false);
+
+
+
 
     const getAnswers = async () => {
         const res = await QuestionService.findById(props.questionSelected.id)
         setAnswers(res.data)
     }
+
+    //quiz se usa para notificar al componente de cambios en el padre(Edition), para poder actualizar las consignas mostradas
+    //En el padre cuando hay una modificacion en las consignas se actualiza el quiz
+    useEffect(getAnswers, [props.quiz]);
+
     const label = { inputProps: { 'aria-label': 'Switch demo' } };
-
-
-
+    console.log('questionAnswer Render')
     const openModal = (a) => {
         setOpen(true)
         setEditA({
@@ -38,6 +44,8 @@ const QuestionAnswer = (props) => {
             id: a.id
         })
     }
+
+
 
     const handleChange = (e) => {
         setEditA({
@@ -55,9 +63,9 @@ const QuestionAnswer = (props) => {
         getAnswers()
         notify('La consigna ha sido borrada')
     }
-    
+
     const edition = async () => {
-        const res = await AnswerService.editAnswer(editA.id, {label: editA.label})
+        const res = await AnswerService.editAnswer(editA.id, { label: editA.label })
         getAnswers()
         closeModal()
         notify('La consigna ha sido editada')
@@ -65,18 +73,18 @@ const QuestionAnswer = (props) => {
 
     const switchModify = (a) => async () => {
         setSwitchState(!a.excluding);
-        const res = await AnswerService.editAnswer(a.id, { excluding: !a.excluding})
+        const res = await AnswerService.editAnswer(a.id, { excluding: !a.excluding })
     }
 
 
     return (
         <div>
             <Modal
-            show={open}
-            onClose={closeModal}
+                show={open}
+                onClose={closeModal}
             >
-                <div style={{display: 'flex', justifyContent: 'center', color: 'black', fontWeight: 'bold', fontSize: 20, padding: 10}}>Editar Consigna</div>
-                <form style={{display: 'flex', justifyContent: 'center'}}>
+                <div style={{ display: 'flex', justifyContent: 'center', color: 'black', fontWeight: 'bold', fontSize: 20, padding: 10 }}>Editar Consigna</div>
+                <form style={{ display: 'flex', justifyContent: 'center' }}>
                     <TextField
                         label="Consigna"
                         margin="normal"
@@ -86,8 +94,8 @@ const QuestionAnswer = (props) => {
                         onChange={handleChange}
                     />
                 </form>
-                <div style={{display: 'flex', justifyContent: 'center'}}>
-                  <Button variant='contained' color='primary'onClick={edition}>Editar</Button>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Button variant='contained' color='primary' onClick={edition}>Editar</Button>
                 </div>
             </Modal>
             <div>
@@ -95,16 +103,16 @@ const QuestionAnswer = (props) => {
                     console.log(a)
                     return (
                         <>
-                        <div style={{ display:'flex', alignItems: 'center', justifyContent:'space-between', flexDirection: 'row' }}>
-                            <div>
-                                <span style={{margin: 5}}>{a.label}</span>
+                            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row' }}>
+                                <div>
+                                    <span style={{ margin: 5 }}>{a.label}</span>
+                                </div>
+                                <div>
+                                    <Switch {...label} onChange={switchModify(a)} defaultChecked={a.excluding} />
+                                    <Button color='primary' style={{ margin: 5, maxWidth: '40px', maxHeight: '40px', minWidth: '40px', minHeight: '40px' }} variant='contained' onClick={() => openModal(a)}><EditIcon style={{ fontSize: 20 }} /></Button>
+                                    <Button color='error' style={{ margin: 5, maxWidth: '40px', maxHeight: '40px', minWidth: '40px', minHeight: '40px' }} onClick={() => deleteAns(a)} variant='contained'><DeleteIcon style={{ fontSize: 20 }} /></Button>
+                                </div>
                             </div>
-                            <div>
-                                <Switch {...label} onChange={switchModify(a)} defaultChecked={a.excluding}/>
-                                <Button color='primary' style={{margin: 5, maxWidth: '40px', maxHeight: '40px', minWidth: '40px', minHeight: '40px'}} variant='contained'onClick={() => openModal(a)}><EditIcon style={{fontSize: 20}}/></Button>
-                                <Button color='error' style={{margin: 5, maxWidth: '40px', maxHeight: '40px', minWidth: '40px', minHeight: '40px'}} onClick={() => deleteAns(a)} variant='contained'><DeleteIcon style={{fontSize: 20}}/></Button>
-                            </div>
-                        </div>
                         </>
                     )
                 })}
