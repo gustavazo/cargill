3ba97730eba7b5dc18e8ef7257d2fa466e35bfda
Ramiro Trovant <32982000+gustavazo@users.noreply.github.com>
2022-07-01 16:06:54 -0300

    b2

--- admin/src/MyLayout.js
+++ admin/src/MyLayout.js
@@ -3,6 +3,6 @@ import { Layout } from 'react-admin';
 import { Menu } from './menu';
 import CustomAppBar from './screens/CustomAppBar';
 
-const MyLayout = (props) => <Layout {...props} appBar={CustomAppBar} menu={Menu}/>;
+const MyLayout = (props) => <Layout {...props} menu={Menu}/>;
 
 export default MyLayout;
