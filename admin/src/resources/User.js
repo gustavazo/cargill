04d4a823f4642cd6187c0be186a9ddabcccd603e
LucasMarelli <lucasmarelli90@gmail.com>
2022-07-12 14:12:03 -0300

    Update User.js
    a) Added inputs validations:
    -All fields are required except gender.
    -Password can't have white spaces
    
    b) Added password edition.
    
    c) Change InputField to PasswordInput component on password field
    
    d) Disable the option superadmin on select input choices if the logged user is not a superadmin

--- admin/src/resources/User.js
+++ admin/src/resources/User.js
@@ -1,8 +1,16 @@
-import { List, Datagrid, TextField, FunctionField } from 'react-admin';
+import { List, Datagrid, TextField, FunctionField, email } from 'react-admin';
 import { SelectInput } from 'react-admin';
-import { ShowButton, EditButton, Edit, SimpleForm, TextInput } from 'react-admin';
+import { ShowButton, EditButton, Edit, SimpleForm, TextInput, PasswordInput, required, regex } from 'react-admin';
 import { Create } from 'react-admin';
-
+const validations = {
+    passwordEdit: [regex(/^\S+$/, "La contraseña no puede tener espacios en blanco")],
+    password: [regex(/^\S+$/, 'La contraseña no puede tener espacios en blanco'), required()],
+    userName: [required()],
+    firstName: [required()],
+    lastName: [required()],
+    email: [required(), email()],
+    type: [required()]
+}
 const redirect = () => `/CustomUsers/`;
 
 const userType = {
@@ -10,7 +18,22 @@ const userType = {
     1: "Administrador",
     2: "Super administrador"
 };
-
+//
+function getUserTypeChoices() {
+    let arrChoices = [];
+    const localUserType = localStorage.getItem("userType");
+    for (const key in userType) {
+        if (userType.hasOwnProperty(key)) {
+            //en caso de que el usuario logeado no sea superadmin no puede asignar la jerarqu[ia de superadmin a ningun usuario]
+            if (key !== '2' || (key === '2' && localUserType === '2')) {
+                const element = { id: key, name: userType[key] };
+                arrChoices.push(element);
+            }
+        }
+    }
+    console.log('ArrChoices', arrChoices);
+    return arrChoices;
+}
 
 export const UserCreate = (props) => (
     <Create {...props}>
@@ -18,15 +41,12 @@ export const UserCreate = (props) => (
             <SelectInput
                 source="type"
                 label="Tipo"
-                choices={[
-                    { id: '0', name: 'Operador' },
-                    { id: '1', name: 'Administrador' },
-                    { id: '2', name: 'Super administrador' }
-                ]} />
-            <TextInput source="firstName" label="Nombre" />
-            <TextInput source="lastName" label="Apellido" />
-            <TextInput source="email" label="Email" />
-            <TextInput source="password" label="Contraseña" />
+                validate={validations.type}
+                choices={getUserTypeChoices()} />
+            <TextInput source="firstName" label="Nombre" validate={validations.firstName} />
+            <TextInput source="lastName" label="Apellido" validate={validations.lastName} />
+            <TextInput source="email" label="Email" validate={validations.email} />
+            <PasswordInput source="password" label="Contraseña" inputProps={{ autoComplete: "new-password" }} validate={validations.password} />
             <SelectInput
                 source="gender"
                 label="Género"
@@ -34,35 +54,37 @@ export const UserCreate = (props) => (
                     { id: 'Masculino', name: 'Masculino' },
                     { id: 'Femenino', name: 'Femenino' }
                 ]} />
-            <TextInput source="username" label="Legajo" />
+            <TextInput source="username" label="Legajo" validate={validations.userName} />
         </SimpleForm>
     </Create>
 );
 
-export const UserEdit = (props) => (
-    <Edit {...props}>
-        <SimpleForm>
-            <SelectInput
-                source="type"
-                label="Tipo"
-                choices={[
-                    { id: '0', name: 'Operador' },
-                    { id: '1', name: 'Administrador' },
-                    { id: '2', name: 'Super administrador' }
-                ]} />
-            <TextInput source="firstName" label="Nombre" />
-            <TextInput source="lastName" label="Apellido" />
-            <SelectInput
-                source="gender"
-                label="Género"
-                choices={[
-                    { id: 'Masculino', name: 'Masculino' },
-                    { id: 'Femenino', name: 'Femenino' }
-                ]} />
-            <TextInput source="username" label="Legajo" />
-        </SimpleForm>
-    </Edit>
-);
+export const UserEdit = (props) => {
+    return (
+        <Edit {...props}>
+            <SimpleForm>
+                <SelectInput
+                    source="type"
+                    label="Tipo"
+                    validate={validations.type}
+                    choices={getUserTypeChoices()} />
+                <TextInput source="firstName" label="Nombre" validate={validations.firstName} />
+                <TextInput source="lastName" label="Apellido" validate={validations.lastName} />
+                <TextInput source="email" label="Email" validate={validations.email} />
+                <PasswordInput source="password" label="Modificar Contraseña" helperText="Completar solo en caso de requerir modificar contraseña" inputProps={{ autoComplete: "new-password" }} validate={validations.passwordEdit} />
+                <SelectInput
+                    source="gender"
+                    label="Género"
+                    choices={[
+                        { id: 'Masculino', name: 'Masculino' },
+                        { id: 'Femenino', name: 'Femenino' }
+                    ]} />
+                <TextInput source="username" label="Legajo" validate={validations.userName} />
+            </SimpleForm>
+        </Edit>
+    )
+}
+    ;
 
 export const UserList = (props) => (
     <List {...props}>
