dd51eb647235302a69dccffaef5ccd33331a216b
LucasMarelli <lucasmarelli90@gmail.com>
2022-07-14 01:01:42 -0300

    BT Create Added Validation

--- admin/src/components/btmodule/create.js
+++ admin/src/components/btmodule/create.js
@@ -23,21 +23,22 @@
 </FormControl> */
 }
 import { Box, Button, MenuItem, Select, TextField } from "@mui/material";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNotify } from "react-admin";
 import AreaService from "../../services/AreaService";
 import BTModuleService from "../../services/BTModuleService";
 
 const create = (props) => {
   console.log("PROPS", props);
+  const [errors, setErrors] = useState({});
   const notify = useNotify();
   const [BTmod, setBTmod] = useState({
     name: props.record.name || "",
     macAddress: props.record.macAddress || "",
     areaId: props.record.areaId || 0,
   });
-
   const [areas, setAreas] = useState([]);
+
 
   useEffect(() => {
     getAreas();
@@ -48,12 +49,58 @@ const create = (props) => {
     setAreas(res.data);
   };
 
-  const handleChange = (e) => {
-    console.log("event", e)
-    const target = e.target
-    if (target.name === 'macAddress') {
-      target.value = target.value.toUpperCase().trim();
+  function fieldsHasErrors() {
+    let errs = {}
+    var fieldsHasErrs = false;
+    for (const key in BTmod) {
+      if (Object.hasOwnProperty.call(BTmod, key)) {
+        const element = BTmod[key];
+        const error = fieldGetError({ name: key, value: element })
+        errs = { ...errs, ...error }
+        fieldsHasErrs |= error[key].value
+      }
+    } {
     }
+    setErrors(errs);
+    return fieldsHasErrs
+  }
+
+
+  //Devuelve un objeto con el nombre del campo indicando si tiene error y el mensaje
+  function fieldGetError(element) {
+    const error = {
+      value: false,
+      msg: ''
+    }
+    switch (element.name) {
+      case 'macAddress':
+        error.value = !(/^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$/.test(element.value))
+        error.msg = 'Debe ser una dirección BT válida'
+        break;
+      case 'name':
+        error.value = !(element.value.length);
+        error.msg = 'Este campo es requerido'
+        break;
+      case 'areaId':
+        error.value = !element.value;
+        error.msg = 'Este campo es requerido'
+        break;
+      default:
+        break;
+    }
+    return { [element.name]: { ...error } }
+  }
+
+  function handleFieldError(target) {
+    console.log('errors', errors)
+    const error = fieldGetError(target)
+    setErrors({ ...errors, ...error })
+
+  }
+
+  const handleChange = (e) => {
+    const target = e.target
+    handleFieldError(target);
     setBTmod({
       ...BTmod,
       [target.name]: target.value,
@@ -61,19 +108,21 @@ const create = (props) => {
   };
 
   const postModule = async () => {
-    const res = props.record.id
-      ? await BTModuleService.update(props.record.id, {
-        name: BTmod.name,
-        macAddress: BTmod.macAddress,
-        areaId: BTmod.areaId,
-      })
-      : await BTModuleService.create({
-        name: BTmod.name,
-        macAddress: BTmod.macAddress,
-        areaId: BTmod.areaId,
-      });
-    notify("Creado");
-    window.location.href = "/#/BTModules";
+    if (!fieldsHasErrors()) {
+      const res = props.record.id
+        ? await BTModuleService.update(props.record.id, {
+          name: BTmod.name,
+          macAddress: BTmod.macAddress.toUpperCase(),
+          areaId: BTmod.areaId,
+        })
+        : await BTModuleService.create({
+          name: BTmod.name,
+          macAddress: BTmod.macAddress.toUpperCase(),
+          areaId: BTmod.areaId,
+        });
+      notify("Creado");
+      window.location.href = "/#/BTModules";
+    }
   };
 
   return (
@@ -95,13 +144,18 @@ const create = (props) => {
           name="name"
           value={BTmod.name}
           onChange={handleChange}
+          error={errors.name?.value}
+          helperText={errors.name?.value ? errors.name?.msg : ""}
         />
+
         <TextField
           label="Direccion"
           margin="normal"
           value={BTmod.macAddress}
           name="macAddress"
           onChange={handleChange}
+          error={errors.macAddress?.value}
+          helperText={errors.macAddress?.value ? errors.macAddress?.msg : ""}
         />
         <Select
           margin="normal"
@@ -109,6 +163,8 @@ const create = (props) => {
           label="Area"
           onChange={handleChange}
           name="areaId"
+          error={errors.areaId?.value}
+          helperText={errors.areaId?.value ? errors.areaId?.msg : ""}
         >
           {areas.length
             ? areas.map((a) => {
