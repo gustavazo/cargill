dacc0f662bcb982d5b42b52046472bfd358a3dc4
LucasMarelli <lucasmarelli90@gmail.com>
2022-07-19 19:28:54 -0300

    Custom AppBar

--- admin/src/MyLayout.js
+++ admin/src/MyLayout.js
@@ -3,6 +3,6 @@ import { Layout } from 'react-admin';
 import { Menu } from './menu';
 import CustomAppBar from './screens/CustomAppBar';
 
-const MyLayout = (props) => <Layout {...props} menu={Menu}/>;
+const MyLayout = (props) => <Layout {...props} menu={Menu} appBar={CustomAppBar}/>;
 
 export default MyLayout;
--- admin/src/screens/CustomAppBar.js
+++ admin/src/screens/CustomAppBar.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import "./appbar.css"
 import { AppBar } from 'react-admin'
-import instalros from  "../images/logo-instalros-1.png"
+import instalros from "../images/logo-instalros-400.png"
+import Typography from '@mui/material/Typography';
 
 const CustomAppBar = (props) => {
-  console.log(props,"que llega para la appbar")
+  console.log("que llega para la appbar", props)
   return (
-        <AppBar {...props} style={{ background: "lightblue"}}>
-            <img src={instalros} className="logo-img" />
-        </AppBar>
+    <AppBar {...props} color={'secondary'} userMenu={false} 
+      sx={{
+        "& .RaAppBar-title": {
+          flex: 1,
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+        },
+      }}>
+      <Typography
+        sx={{ flex: 1 }}
+        variant="h6"
+        color="inherit"
+        id='react-admin-title' />
+      <img src={instalros} className="logo-img" />
+
+    </AppBar>
   )
 }
 
